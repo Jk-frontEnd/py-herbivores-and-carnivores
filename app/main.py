3264f6390c441849543856ddcaff1b5f955099e9
JulianskyMe <juliakamynskaya@gmail.com>
2024-12-22 23:00:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,37 @@
-# write your code here
+class Animal:
+    animal = []
+    
+    def __init__(self, name, hidden=False, health=100):
+        self.health = health
+        self.name = name
+        self.hidden = hidden
+        Animal.alive.append(self)
+        
+    def health_stats(self):
+        if (self.health <= 0):
+            self.health = 0
+            Animal.remove_dead()
+
+    @classmethod
+    def remove_dead(cls):
+        cls.alive = [animal for animal in cls.alive if animal.health == 0]
+    
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+        
+
+
+class Herbivore(Animal):
+    def hide(self):
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore):
+        if isinstance(herbivore, Carnivore):
+            return "Carnivore cannot bite another carnivor"
+        elif herbivore.hidden == True:
+            return "Carnivore cannot bite hidden herbivore"
+        else:
+            herbivore.health -= 50
+            herbivore.health_stats()
