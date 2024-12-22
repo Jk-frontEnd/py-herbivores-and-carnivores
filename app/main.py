585d2ba0992ef9989d369332a6b322b5170f732d
JulianskyMe <juliakamynskaya@gmail.com>
2024-12-22 23:07:48 +0200

    upd

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 class Animal:
-    animal = []
+    alive = []
     
     def __init__(self, name, hidden=False, health=100):
         self.health = health
@@ -14,7 +14,7 @@ class Animal:
 
     @classmethod
     def remove_dead(cls):
-        cls.alive = [animal for animal in cls.alive if animal.health == 0]
+        cls.alive = [animal for animal in cls.alive if animal.health > 0]
     
     def __repr__(self):
         return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
@@ -30,8 +30,7 @@ class Carnivore(Animal):
     def bite(self, herbivore):
         if isinstance(herbivore, Carnivore):
             return "Carnivore cannot bite another carnivor"
-        elif herbivore.hidden == True:
+        elif herbivore.hidden:
             return "Carnivore cannot bite hidden herbivore"
-        else:
-            herbivore.health -= 50
-            herbivore.health_stats()
+        herbivore.health -= 50
+        herbivore.health_stats()
